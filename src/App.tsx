3dd652e19a2b935cde31d898bcba0f286a7ca092
CoolSpring8 <coolspring888@gmail.com>
2021-08-09 18:59:05 +0800

    set up prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App(): JSX.Element {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+  return <div>Hello world!</div>;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
